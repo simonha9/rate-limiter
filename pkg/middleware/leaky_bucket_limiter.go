2d33cef4ed50b67355138f8ccc37cc8d5c58a722
Simon H <simon9ha@gmail.com>
2023-09-19 15:05:31 -0400

    feat: add leaky bucket algo

--- pkg/middleware/leaky_bucket_limiter.go
+++ pkg/middleware/leaky_bucket_limiter.go
@@ -4,62 +4,46 @@ import (
 	"github.com/gin-gonic/gin"
 	"time"
 	"net/http"
-	"github.com/simonha9/rate-limiter/pkg"
 )
 
 // need to implement gin handlerfunc interface for it to be a middleware
-func (t LeakyBucketLimiter) TokenLimiterMiddleware() gin.HandlerFunc {
+func (t LeakyBucketLimiter) LeakyBucketLimiterMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		if (c.Request.URL.Path == "/ping") {
-
-			if (t.buckets[0].NumTokens > 0) {
-				t.buckets[0].NumTokens--
-			} else if (t.buckets[1].NumTokens > 0) {
-				t.buckets[1].NumTokens--
-			} else {
-				// raise a rate limit error
-				c.AbortWithStatusJSON(http.StatusTooManyRequests, gin.H{
-					"message": "Rate limit exceeded",
-					"retry-after": "1",
-				})
-				return
-			}
+		// check if queue is full
+		if (len(t.queue) == t.capacity) {
+			// raise a rate limit error
+			c.AbortWithStatusJSON(http.StatusTooManyRequests, gin.H{
+				"message": "Rate limit exceeded",
+				"retry-after": "1",
+			})
+			return
 		}
+		
 		c.Next()
 	}
 }
 
 type LeakyBucketLimiter struct {
-	refillRate int
-	buckets []pkg.Bucket
-	numBuckets int
+	processRate int
+	queue chan int
+	capacity int
 }
 
-func NewLeakyBucketLimiter() LeakyBucketLimiter {
+func NewLeakyBucketLimiter(capacity int) LeakyBucketLimiter {
 	return LeakyBucketLimiter{
-		refillRate: 1,
-		buckets: []pkg.Bucket{
-			pkg.Bucket{
-				Capacity: 10,
-				NumTokens: 0,
-			},
-			pkg.Bucket{
-				Capacity: 10,
-				NumTokens: 0,
-			},
-		},
+		processRate: 1,
+		capacity: capacity,
+		queue: make(chan int, capacity),
 	}
 }
 
-func (t LeakyBucketLimiter) refill() {
-	for i := 0; i < t.numBuckets; i++ {
-		t.buckets[i].NumTokens = t.buckets[i].Capacity
-	}
+func (t LeakyBucketLimiter) process() {
+	<- t.queue
 }
 
 func (t LeakyBucketLimiter) Run() {
 	for {
-		t.refill()
-		time.Sleep(time.Duration(t.refillRate) * time.Second)
+		t.process()
+		time.Sleep(time.Duration(t.processRate) * time.Second)
 	}
 }
